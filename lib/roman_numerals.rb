2606446f314720e87d8459d71bd5afa75bbca5b1
sarahabimay <sarahabimay@gmail.com>
2016-01-27 09:30:50 +0000

    Conditional to While transformation

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -3,22 +3,9 @@ class RomanNumerals
   def convert(from_arabic) 
     roman = ""
     arabic = from_arabic
-
-    if arabic >=3
-      roman += "III"
-      arabic -= from_arabic
-    end
-    if arabic >= 2
-      roman += "II"
-      arabic -= from_arabic
-    end
-    if arabic >= 1
+    while(arabic > 0)
       roman += "I"
-      arabic -= from_arabic
-    end
-    if arabic >= 0
-      roman += ""
-      arabic -= from_arabic
+      arabic -= 1
     end
     roman
   end
