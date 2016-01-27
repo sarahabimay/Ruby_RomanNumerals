c7a74cc6ce124ced08576de75be859d4e09cb93e
sarahabimay <sarahabimay@gmail.com>
2016-01-27 09:13:44 +0000

    First three tests showing duplication

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,6 +1,17 @@
 class RomanNumerals
  
   def convert(from_arabic) 
-    return ""
+    if from_arabic == 3
+      return "III"
+    end
+    if from_arabic == 2
+      return "II"
+    end
+    if from_arabic == 1
+      return "I"
+    end
+    if from_arabic == 0
+      return ""
+    end
   end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe RomanNumerals do
   it "converts 0 to empty space" do
     expect(romans.convert(0)).to eq("")
   end
+
+  it "converts 1 to I"do
+    expect(romans.convert(1)).to eq("I")
+  end
+
+  it "converts 2 to II"do
+    expect(romans.convert(2)).to eq("II")
+  end
+
+  it "converts 3 to III"do
+    expect(romans.convert(3)).to eq("III")
+  end
 end
